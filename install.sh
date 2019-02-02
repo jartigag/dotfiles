9ef85fe2f3d63320ba13c264621c3086fb786ee8
jartigag <jartigag@pm.me>
2019-02-02 17:29:22 +0100

    added: install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+uthor: Javier Artiga Garijo (v0.5)
+#date: 02/02/2019
+#version: 0.1 
+#
+# install my configuration files (dotfiles), just with symbolic links 
+# usage: bash install.sh
+
+echo "let's install some dotfiles.."
+
+# 1. get all dotfiles into an array:
+# (adapting an example from http://mywiki.wooledge.org/BashGuide/Arrays)
+#dotfiles=()
+#while read -r -d $'\0'; do
+#       dotfiles+=("$REPLY")
+#done < <(find ./ -type f -name ".*")
+
+dotfiles=($(find `pwd` -type f -name ".*"))
+
+for i in ${!dotfiles[@]}
+	#The keys are accessed using an exclamation point: ${!array[@]}, 
+	#the values are accessed using ${array[@]}
+do
+	        echo "ln -sf ${dotfiles[$i]} $HOME"
+		        ln -sf ${dotfiles[$i]} $HOME
+		done
+
+		echo "done."
