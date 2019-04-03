f7648c5bf49ecac92524c14b83b4ea2a468d919b
jartigag <33371871+jartigag@users.noreply.github.com>
2019-04-03 10:47:25 +0200

    bring_spotify_to_front.sh v0.2

--- bring_spotify_to_front.sh
+++ bring_spotify_to_front.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 #author: @jartigag
-#date: 22/03/2019
-#version: 0.1
+#date: 03/04/2019
+#version: 0.2
+#
+# i associate 'alt+s' shortcut with this script,
+# so it always brings spotify's window to front
+# when i press it.
 
 if [ ! -f /tmp/spotify-winID.wid ]; then
+	spotify &&
 	wmctrl -l | grep "Spotify" | cut -d " " -f1 >> /tmp/spotify-winID.wid
 	cat /tmp/spotify-winID.wid
 else
