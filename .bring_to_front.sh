cee29bfd2441d34326f3c119d527b5648b626b25
Javier Artiga <javier.artiga@naudit.es>
2019-07-31 14:00:25 +0200

    .bring_to_front.sh v0.5

--- .bring_to_front.sh
+++ .bring_to_front.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 #author: @jartigag
-#date: 2019-05-10
-#version: 0.4
+#date: 2019-07-31
+#version: 0.5
 #
 # i associate keyboard shortcuts with (this script + different arguments),
 # so it brings desired windows to front when i press each one.
 #
-# usage: bash bring_to_front.sh "Spotify" /tmp/spotify-winID.wid
+# usage: bash .bring_to_front.sh "Spotify" /tmp/spotify-winID.wid
+#
+# requeriments: sudo apt install wmctrl
 
 windowTitle=$1
 widFile=$2
 
 if [ ! -f $widFile ]; then
-	flagDone=`wmctrl -l | grep $windowTitle | cut -d " " -f1`
-	while [ -z "$flagDone" ]
-	do
-		flagDone=`wmctrl -l | grep $windowTitle | cut -d " " -f1`
-	done
-	echo "$flagDone" > $widFile 
-else
-	wmctrl -ia $(< $widFile)
+        flagDone=`wmctrl -l | grep $windowTitle | cut -d " " -f1`
+        while [ -z "$flagDone" ]
+        do
+                flagDone=`wmctrl -l | grep $windowTitle | cut -d " " -f1`
+        done
+        echo "$flagDone" > $widFile 
 fi
+
+wmctrl -ia $(< $widFile)
